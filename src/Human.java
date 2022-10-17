b384d310169e2afddb58ff4dd39e278b71334036
zemscky <aleksei.zemscky@yandex.ru>
2022-10-17 14:19:51 +0300

    Домашнее задание 1. Задание 2.

--- src/Human.java
+++ src/Human.java
@@ -2,14 +2,17 @@ public class Human {
     private int yearOfBirth;
     private String name;
     private String town;
+     private String job;
 
-    public Human(int yearOfBirth, String name, String town) {
+    public Human(int yearOfBirth, String name, String town, String job) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.town = town;
+        this.job = job;
     }
 
     void talk () {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town +
+                ". Я родился в " + yearOfBirth + " году. Я работаю на должности: " + job + ". Будем знакомы!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human(35,"Максим", "Минск");
-        Human anna = new Human(29, "Аня", "Москва");
-        Human katia = new Human(28, "Катя", "Калининград");
-        Human artem = new Human(27, "Артем", "Москва");
+        Human maksim = new Human(35,"Максим", "Минск","Бренд-менеджера");
+        Human anna = new Human(29, "Аня", "Москва", "Методиста");
+        Human katia = new Human(28, "Катя", "Калининград", "Продакт-менеджера");
+        Human artem = new Human(27, "Артем", "Москва", "Директора по развитию бизнеса");
 
         maksim.talk();
         anna.talk();

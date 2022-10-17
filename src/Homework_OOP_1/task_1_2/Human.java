a1cbed478d7261260379ba083fe00aae9aaead56
zemscky <aleksei.zemscky@yandex.ru>
2022-10-17 19:36:53 +0300

    Домашнее задание 3. Задание 1.

--- src/Homework_OOP_1/task_1_2/Human.java
+++ src/Homework_OOP_1/task_1_2/Human.java
@@ -7,14 +7,32 @@ public class Human {
      private String job;
 
     public Human(int yearOfBirth, String name, String town, String job) {
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
-        this.town = town;
-        this.job = job;
+
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (job == null) {
+            this.job = "Информация не указана";
+        } else {
+            this.job = job;
+        }
     }
 
     void talk () {
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + town +
                 ". Я родился в " + yearOfBirth + " году. Я работаю на должности: " + job + ". Будем знакомы!");
     }
+
 }
